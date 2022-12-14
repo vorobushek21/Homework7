a9bf87b14d72be83d7d9248bfad5f6655e4e5160
Anton <anton.vorobev1999@mail.ru>
2022-12-14 19:47:17 +0300

    Выполнил задачи с 1 по 8

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,126 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+        task8();
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        int monthCash = 15_000;
+        int cash = 0;
+        int month = 0;
+        while (cash <= 2_459_000) {
+            cash = cash + monthCash;
+            month++;
+        }
+        System.out.println("Накопления в "+month+" месяце составляют - "+cash);
+    }
+
+    public static void task2() {
+        System.out.println("Задача 2");
+        int i = 0;
+        while (i<10){
+            i++;
+            System.out.print(i+" ");
+        }
+        System.out.println();
+        for(i=10;i!=0;i--){
+            System.out.print(i+" ");
+        }
+        System.out.println();
+    }
+
+    public static void task3() {
+        System.out.println("Задача 3");
+        int population = 12_000_000;
+        int deathPerThousand = 8;
+        int birthPerThousand = 17;
+        int year = 1;
+        while (year<=10){
+            int yearRatio = population/1000;
+            int deathPerYear = deathPerThousand*yearRatio;
+            int birthPerYear = birthPerThousand*yearRatio;
+            population = (population+birthPerYear)-deathPerYear;
+            System.out.println("Год - "+year+", численность населения составляет - "+population);
+            year++;
+        }
+    }
+
+    public static void task4() {
+        System.out.println("Задача 4");
+        int percent = 7;
+        float money = 15_000f;
+        int month = 0;
+        while (money<12_000_000){
+            float moneyPerMonth = (money/100)*percent;
+            money = money+moneyPerMonth;
+            month++;
+        }
+        System.out.println("Месяц - "+month+", накопленная сумма - "+money);
+    }
+
+    public static void task5() {
+        System.out.println("Задача 5");
+        int percent = 7;
+        float money = 15_000f;
+        int month = 0;
+        while (money<12_000_000){
+            float moneyPerMonth = (money/100)*percent;
+            money = money+moneyPerMonth;
+            month++;
+            if (month%6 == 0){
+                System.out.println("Месяц - "+month+", накопленная сумма - "+money);
+            }
+        }
+    }
+
+    public static void task6() {
+        System.out.println("Задача 6");
+        int percent = 7;
+        float money = 15_000f;
+        int month = 0;
+        int year = 9;
+        int period = year * 12;
+        while (month<=period){
+            float moneyPerMonth = (money/100)*percent;
+            money = money+moneyPerMonth;
+            month++;
+            if (month%6 == 0){
+                System.out.println("Прошло полгода, "+month+" месяцев, накопленная сумма - "+money);
+            }
+        }
+    }
+
+    public static void task7() {
+        System.out.println("Задача 7");
+        int percent = 7;
+        float money = 15_000f;
+        int month = 0;
+        int year = 9;
+        int period = year * 12;
+        while (month<=period){
+            float moneyPerMonth = (money/100)*percent;
+            money = money+moneyPerMonth;
+            month++;
+            if (month%6 == 0){
+                System.out.println("Прошло полгода, "+month+" месяцев, накопленная сумма - "+money);
+            }
+        }
+    }
+
+    public static void task8() {
+        System.out.println("Задача 8");
+        int year = 1822;
+        for (;year<=2122; year++){
+            if (year%79==0){
+                System.out.println(year);
+            }
+        }
     }
 }
